8a9e81eaa2b1e8c42e8bd22a429f0453a91024d5
Siddharth <siddharth@Siddharths-MacBook-Air.local>
2024-08-02 17:41:08 +0530

    fix: route issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,14 @@
 import "./app.scss";
-import CursorComp from "./components/cursor/Cursor";
 import HomeScreen from "./container/screens/Home/HomeScreen";
-import NavBarScreen from "./container/screens/navbar/navbar";
-import ParallaxScreen from "./container/screens/Parallax/Parallax";
-import ServicesScreen from "./container/screens/services/Services";
-import PortfolioScreen from "./container/screens/Portfolio/Portfolio";
-import ContactScreen from "./container/screens/contact/Contact";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div>
-        <CursorComp />
-        <section id="HomePage">
-          <NavBarScreen />
-          <HomeScreen />
-        </section>
-        <section id="Services">
-          <ParallaxScreen type="services" />
-        </section>
-        <section>
-          <ServicesScreen />
-        </section>
-        <section id="Portfolio">
-          <ParallaxScreen type="portfolio" />
-        </section>
-        <PortfolioScreen />
-        <section id="Contact">
-          <ContactScreen />
-        </section>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/container/screens/Home/HomeScreen.tsx
+++ src/container/screens/Home/HomeScreen.tsx
@@ -1,4 +1,10 @@
 import Image from "../../../assets/about.png";
+import CursorComp from "../../../components/cursor/Cursor";
+import ContactScreen from "../contact/Contact";
+import NavBarScreen from "../navbar/navbar";
+import ParallaxScreen from "../Parallax/Parallax";
+import PortfolioScreen from "../Portfolio/Portfolio";
+import ServicesScreen from "../services/Services";
 import "./HomeScreen.scss";
 import { motion } from "framer-motion";
 
@@ -41,44 +47,66 @@ const sliderVariants: any = {
 
 function HomeScreen() {
   return (
-    <div className="homePage">
-      <div className="wrapper">
-        <motion.div
-          className="textContainer"
-          variants={textVariants}
-          initial="initial"
-          animate="animate"
-        >
-          <motion.h2 variants={textVariants}>Siddharth Jain</motion.h2>
-          <motion.h1 variants={textVariants}>
-            passionate frontend developer
-          </motion.h1>
-          {/* <motion.div variants={textVariants} className="buttons">
+    <>
+      <CursorComp />
+      <div>
+        <section id="HomePage">
+          <NavBarScreen />
+          <div className="homePage">
+            <div className="wrapper">
+              <motion.div
+                className="textContainer"
+                variants={textVariants}
+                initial="initial"
+                animate="animate"
+              >
+                <motion.h2 variants={textVariants}>Siddharth Jain</motion.h2>
+                <motion.h1 variants={textVariants}>
+                  passionate frontend developer
+                </motion.h1>
+                {/* <motion.div variants={textVariants} className="buttons">
             <motion.button variants={textVariants}>
               See the latest works
             </motion.button>
             <motion.button variants={textVariants}>Contact Me</motion.button>
           </motion.div> */}
-          <motion.img
-            variants={textVariants}
-            animate="scrollButtons"
-            src="/public/scroll.png"
-            alt="scroll"
-          />
-        </motion.div>
+                <motion.img
+                  variants={textVariants}
+                  animate="scrollButtons"
+                  src="/public/scroll.png"
+                  alt="scroll"
+                />
+              </motion.div>
+            </div>
+            <motion.div
+              className="sliderTextContainer"
+              variants={sliderVariants}
+              initial="initial"
+              animate="animate"
+            >
+              Freelancer or Full Time Software developer
+            </motion.div>
+            <div className="imageContainer">
+              <img src={Image} alt="logo" />
+            </div>
+          </div>
+          {/* <HomeScreen /> */}
+        </section>
+        <section id="Services">
+          <ParallaxScreen type="services" />
+        </section>
+        <section>
+          <ServicesScreen />
+        </section>
+        <section id="Portfolio">
+          <ParallaxScreen type="portfolio" />
+        </section>
+        <PortfolioScreen />
+        <section id="Contact">
+          <ContactScreen />
+        </section>
       </div>
-      <motion.div
-        className="sliderTextContainer"
-        variants={sliderVariants}
-        initial="initial"
-        animate="animate"
-      >
-        Freelancer or Full Time Software developer
-      </motion.div>
-      <div className="imageContainer">
-        <img src={Image} alt="logo" />
-      </div>
-    </div>
+    </>
   );
 }
 
